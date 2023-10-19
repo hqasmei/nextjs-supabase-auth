2b458edd0c385c0692df0a7f7b53326d0559b174
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-18 19:48:34 -0700

    Go back to home page

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -19,5 +19,5 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin + "/dashboard");
+  return NextResponse.redirect(requestUrl.origin);
 }
