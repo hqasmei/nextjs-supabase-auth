94cff5a3040dc8e3fd5bbc3fe36d5637b4d42afd
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-18 19:02:52 -0700

    Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return <div className="text-white items-center justify-center flex w-full flex-1">DashboardPage</div>;
+};
+
+export default DashboardPage;
---
+++ src/app/login/messages.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+
+import { useSearchParams } from "next/navigation";
+
+import { getURL } from "@/lib/utils";
+
+const Messages = () => {
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+  const message = searchParams.get("message");
+
+  return (
+    <>
+      {error && (
+        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
+          {error}
+        </p>
+      )}
+      {message && (
+        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
+          {message}
+        </p>
+      )}
+    </>
+  );
+};
+
+export default Messages;
